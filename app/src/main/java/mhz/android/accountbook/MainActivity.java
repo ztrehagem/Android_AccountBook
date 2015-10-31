2599d0f3841328ab40b77979366868dde09b3a9e
MHz <megahertz.556@gmail.com>
2015-11-01 00:36:14 +0900

    ViewPager、動かん

--- app/src/main/java/mhz/android/accountbook/MainActivity.java
+++ app/src/main/java/mhz/android/accountbook/MainActivity.java
@@ -1,5 +1,6 @@
 package mhz.android.accountbook;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((ViewPager) findViewById(R.id.pager)).setAdapter( new MainFragmentStatePagerAdapter(getSupportFragmentManager()));
     }
 }
