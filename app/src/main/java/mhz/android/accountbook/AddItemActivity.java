f3a1661793ca7111ce94e1d2c310266e94e594d2
MHz <megahertz.556@gmail.com>
2015-11-01 21:03:36 +0900

    AddItemActivity上でのアイテム追加　一旦可能に

--- app/src/main/java/mhz/android/accountbook/AddItemActivity.java
+++ app/src/main/java/mhz/android/accountbook/AddItemActivity.java
@@ -2,6 +2,14 @@ package mhz.android.accountbook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import mhz.android.accountbook.db.MySQLiteController;
+import mhz.android.accountbook.list.Item;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -9,5 +17,32 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        Button button = (Button)findViewById(R.id.button_addItem);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePicker datePicker = (DatePicker)findViewById(R.id.datePicker);
+                int year = datePicker.getYear();
+                int month = datePicker.getMonth() + 1;
+                int day = datePicker.getDayOfMonth();
+
+                int genreId = 1; // default
+
+                String title = ((EditText)findViewById(R.id.input_title)).getText().toString();
+
+                String amount_str = ((EditText)findViewById(R.id.input_amount)).getText().toString();
+                if( amount_str.equals("") ){
+                    Toast.makeText(getApplicationContext(), R.string.errorMsg_amountIsEmpty, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                int amount = Integer.parseInt(amount_str);
+
+                MySQLiteController.getInstance().addItem( year, month, day, genreId, title, amount );
+
+                Toast.makeText(getApplicationContext(), R.string.successMsg_ItemAdded, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
