abcad2d1aa02b1817d56a76849fca24d7578d5fe
MHz <megahertz.556@gmail.com>
2015-11-01 23:14:57 +0900

    DB：Genreの全レコードを取り出すメソッド

--- app/src/main/java/mhz/android/accountbook/db/MySQLiteController.java
+++ app/src/main/java/mhz/android/accountbook/db/MySQLiteController.java
@@ -4,6 +4,8 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.util.Pair;
 
 import java.util.ArrayList;
 
@@ -46,14 +48,14 @@ public class MySQLiteController {
         v.put( "genre_id", genreId );
         v.put( "title", title );
         v.put( "amount", amount );
-        db.insert( "Items", null, v );
+        db.insert("Items", null, v);
     }
 
     public ArrayList<Item> getItemsForListView() {
         StringBuilder sql = new StringBuilder("");
-        sql.append( "select Items.year, Items.month, Items.day, Items.title, Items.amount, Genre.name, Genre.r, Genre.g, Genre.b " );
-        sql.append( "from Items " );
-        sql.append( "left outer join Genre on Items.genre_id = Genre.id;" );
+        sql.append("select Items.year, Items.month, Items.day, Items.title, Items.amount, Genre.name, Genre.r, Genre.g, Genre.b ");
+        sql.append("from Items ");
+        sql.append("left outer join Genre on Items.genre_id = Genre.id;");
         Cursor c = db.rawQuery( sql.toString(), null );
 
         ArrayList<Item> list = new ArrayList<>();
@@ -70,6 +72,32 @@ public class MySQLiteController {
                         (byte)c.getInt(c.getColumnIndex("r")),
                         (byte)c.getInt(c.getColumnIndex("g")),
                         (byte)c.getInt(c.getColumnIndex("b"))
+                ));
+            } while( c.moveToNext() );
+        }
+
+        c.close();
+
+        return list;
+    }
+
+    public ArrayList< Pair< Integer, Pair< String, Integer > > > getAllGenre() {
+        Cursor c = db.query("Genre", null, null, null, null, null, "id asc");
+
+        ArrayList< Pair< Integer, Pair< String, Integer > > > list = new ArrayList<>();
+
+        if( c.moveToFirst() ) {
+            do {
+                list.add( new Pair<>(
+                        c.getInt(c.getColumnIndex("id")),
+                        new Pair<>(
+                                c.getString(c.getColumnIndex("name")),
+                                Color.rgb(
+                                        c.getInt(c.getColumnIndex("r")),
+                                        c.getInt(c.getColumnIndex("g")),
+                                        c.getInt(c.getColumnIndex("b"))
+                                )
+                        )
                 ));
             } while( c.moveToNext() );
         }
